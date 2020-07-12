4206f220f04397ce4d2723395ca8aaf782c52e02
Şeyda Arı <ariseydaari@gmail.com>
2020-07-13 00:08:37 +0300

    Update wordarray.c

--- wordarray.c
+++ wordarray.c
@@ -2,6 +2,12 @@
 #include<string.h>
 #define size 100
 
+//Write a program that asks the user to enter a sentence and separates this sentence into words and saves the words into a string array. 
+//The sentence can be limited to 20 words. 
+//Then save the words in a text file, one after the other. 
+//The user may or may not have placed a period at the end of the sentence. 
+//The code you write must provide both conditions.
+
 int main(){
 	
 	FILE *f=fopen("words.txt","w");
